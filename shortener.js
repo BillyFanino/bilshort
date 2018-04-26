65827c28d8bb21fdb5c38fab0e37a47f7a4e67c2
billy_fanino <billy.fanino24@gmail.com>
2018-04-26 09:10:49 +0700

    revision db

--- shortener.js
+++ shortener.js
@@ -2,7 +2,7 @@ const Hapi     = require('hapi');
 const server   = new Hapi.Server();  
 const routes   = require('./routes');  
 const mongoose = require('mongoose');  
-const mongoUri = process.env.MONGOLAB_URI || 'mongodb://copzslowdown:Akuganteng123@ds145359.mlab.com:45359/bilshort';  
+const mongoUri = process.env.MONGOLAB_URI || 'mongodb://copz:ssmaju123@ds145359.mlab.com:45359/bilshort';  
 // If you're testing this locally, change mongoUri to:
 // 'mongodb://localhost:27017/shortio'
 
